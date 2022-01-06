30641a51e0706726699112957c9cfe96e6dc9923
sh1745 <ish794285@gmail.com>
2022-01-06 22:52:05 +0900

    mylist-contact-ver3.2

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -6,104 +6,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ContactController {
 
-  Contact[] contacts = new Contact[5];
-  int size = 0;
-
-
   @RequestMapping("/contact/list")
   public Object list() {
-    Contact[] arr = new Contact[size];
-    for (int i = 0; i < size; i++) {
-      arr[i] = contacts[i];
-    }
-    return arr;
+    return ArrayList.toArray();
   }
 
   @RequestMapping("/contact/add")
   public Object add(Contact contact) {
-    System.out.println(contact);
-
-    if (size == contacts.length) {
-      contacts = grow();
-    }
-
-    contacts[size++] = contact;
-
-    return size;
+    //    System.out.println(contact);
+    ArrayList.add(contact);
+    return ArrayList.size;
   }
 
 
   @RequestMapping("/contact/get")
   public Object get(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if (index == -1) {
       return "";
     }
 
-    return contacts[index];
+    return ArrayList.contacts[index];
   }
 
   @RequestMapping("/contact/update")
   public Object update(Contact contact) {
-    int index = indexOf(contact.email);
+    int index = ArrayList.indexOf(contact.email);
     if (index == -1) {
       return 0;
     }
 
-    contacts[index] = contact;
-    return 1;
+    return ArrayList.set(index, contact) == null ? 0 : 1;
   }
 
   @RequestMapping("/contact/delete")
   public Object delete(String email) {
-    int index = indexOf(email);
+    int index = ArrayList.indexOf(email);
     if (index == -1) {
       return 0;
     }
 
-    remove(index);
+    ArrayList.remove(index);
     return 1;
   }
-
-  int indexOf(String email) {
-    for (int i = 0; i < size; i++) {
-      Contact contact = contacts[i];
-      if (contact.email.equals(email)) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  Contact remove(int index) {
-    Contact old = contacts[index];
-    for (int i = index + 1; i < size; i++) {
-      contacts[i - 1] = contacts[i];
-    }
-    size--;
-    return old;
-  }
-
-  Contact[] grow() {
-    Contact[] arr = new Contact[newLength()];
-    copy(contacts, arr);
-    return arr;
-  }
-
-
-  int newLength() {
-    return contacts.length + (contacts.length >> 1);
-  }
-
-
-  void copy(Contact[] source, Contact[] target) {
-    int length = source.length;
-    if (target.length < source.length) {
-      length = target.length;
-    }
-    for (int i = 0; i < length; i++) {
-      target[i] = source[i];
-    }
-  }
-
 }
