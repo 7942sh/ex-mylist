46107767cb8dc5c59f222c9cdd3036688809ddfb
sh1745 <ish794285@gmail.com>
2022-02-08 21:44:07 +0900

    mylist-BoardDao-ver8.2(2)

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/CsvBoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/CsvBoardDao.java
@@ -5,10 +5,12 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import org.springframework.stereotype.Repository;
 import com.eomcs.mylist.domain.Board;
 import com.eomcs.util.ArrayList;
 
-public class CsvBoardDao {
+@Repository
+public class CsvBoardDao implements BoardDao {
   ArrayList boardList = new ArrayList(); // 변수 선언 = 변수를 만들라는 명령!
 
   public CsvBoardDao() {
@@ -17,7 +19,7 @@ public class CsvBoardDao {
 
       String csvStr;
       while ((csvStr = in.readLine()) != null) {
-        boardList.add(Board.valueOf(csvStr)); 
+        boardList.add(Board.valueOf(csvStr));
       }
 
       in.close();
@@ -38,19 +40,23 @@ public class CsvBoardDao {
     out.close();
   }
 
+  @Override
   public int countAll() {
     return boardList.size();
   }
 
+  @Override
   public Object[] findAll() {
     return boardList.toArray();
   }
 
+  @Override
   public void insert(Board board) throws Exception {
     boardList.add(board);
     save();
   }
 
+  @Override
   public Board findByNo(int no) {
     if (no < 0 || no >= boardList.size()) {
       return null;
@@ -58,6 +64,7 @@ public class CsvBoardDao {
     return (Board) boardList.get(no);
   }
 
+  @Override
   public int update(int no, Board board) throws Exception {
     if (no < 0 || no >= boardList.size()) {
       return 0;
@@ -67,6 +74,7 @@ public class CsvBoardDao {
     return 1;
   }
 
+  @Override
   public int delete(int no) throws Exception {
     if (no < 0 || no >= boardList.size()) {
       return 0;
@@ -76,20 +84,10 @@ public class CsvBoardDao {
     return 1;
   }
 
+  @Override
   public void increaseViewCount(int no) throws Exception {
     Board board = findByNo(no);
     board.setViewCount(board.getViewCount() + 1);
     save();
   }
 }
-
-
-
-
-
-
-
-
-
-
-
